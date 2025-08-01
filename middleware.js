4d0f070407b4bfacedab4d081e87afc57e11df7a
ashmit1795 <ashmitpatra77@gmail.com>
2025-08-02 01:42:14 +0530

    feat: update middleware to protect routes and redirect unauthenticated users to sign-in

--- middleware.js
+++ middleware.js
@@ -1,11 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/", "/api/inngest(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+	"/dashboard(.*)",
+	"/expenses(.*)",
+	"/groups(.*)",
+	"/settlements(.*)",
+	"/contacts(.*)",
+	"/person(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-	if (!isPublicRoute(req)) {
-		await auth.protect();
+	const { userId } = await auth();
+	if (!userId && isProtectedRoute(req)) {
+		const { redirectToSignIn } = await auth();
+		return redirectToSignIn();
 	}
+	
+	return NextResponse.next();
 });
 
 export const config = {
